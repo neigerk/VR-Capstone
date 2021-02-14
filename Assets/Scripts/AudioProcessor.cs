c68e77a7c90b99a7f989b2f7e54e71455cbf923c
neigerk <neigerk@oregonstate.edu>
2021-02-14 02:36:53 -0800

    Merged Kevin's Stuff
    I added my scripts, my scene, prefabs, etc.  I believe everything I made has been merged

--- Assets/Scripts/AudioProcessor.cs
+++ Assets/Scripts/AudioProcessor.cs
@@ -405,4 +405,5 @@ public class AudioProcessor : MonoBehaviour
 			}
 			return sum / del_length;
 		}
-	
+	}
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerStats : MonoBehaviour
+{
+     public TextMesh textHp;
+     public TextMesh textEnergy;
+
+     [SerializeField]
+     private int maxHp;
+     private int currentHp;
+
+     [SerializeField]
+     private int maxEnergy;
+     private int currentEnergy;
+
+     public event Action<int, int> OnHpPctChanged = delegate { };
+     public event Action<int, int> OnEnergyPctChanged = delegate { };
+
+     private void Awake()
+     {
+          currentHp = maxHp;
+          currentEnergy = maxEnergy / 2;
+     }
+
+     // Start is called before the first frame update
+     void Start()
+    {
+          ChangeCurrentEnergy(0);
+          ChangeCurrentHP(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          //SetHPText();
+          //SetEnergyText();
+          if (Input.GetKeyDown(KeyCode.LeftControl))
+               ChangeCurrentHP(-10);
+          if (Input.GetKeyDown(KeyCode.LeftAlt))
+               ChangeCurrentEnergy(-10);
+     }
+
+     //TEXT FUNCTIONS
+     void SetHPText()
+     {
+          textHp.text = currentHp.ToString() + " / " + maxHp.ToString();
+     }
+
+     void SetEnergyText()
+     {
+          textEnergy.text = currentEnergy.ToString() + " / " + maxEnergy.ToString();
+     }
+
+     //PUBLIC GETTERS AND SETTERS
+     public int GetCurrentHP()
+     {
+          return currentHp;
+     }
+     public int GetMaxHP()
+     {
+          return maxHp;
+     }
+     public int GetCurrentEnergy()
+     {
+          return currentEnergy;
+     }
+     public int GetMaxEnergy()
+     {
+          return maxEnergy;
+     }
+     public void SetCurrentHP(int value)
+     {
+          currentHp = value;
+     }
+     public void SetCurrentEnergy(int value)
+     {
+          currentEnergy = value;
+     }
+     public void ChangeCurrentHP(int value)
+     {
+          currentHp = currentHp + value;
+          if (currentHp < 0)
+          {
+               currentHp = 0;
+          }
+          if (currentHp > maxHp)
+          {
+               currentHp = maxHp;
+          }
+
+          OnHpPctChanged(currentHp, maxHp);
+     }
+     public void ChangeCurrentEnergy(int value)
+     {
+          currentEnergy = currentEnergy + value;
+          if(currentEnergy < 0)
+          {
+               currentEnergy = 0;
+          }
+          if(currentEnergy > maxEnergy)
+          {
+               currentEnergy = maxEnergy;
+          }
+
+          OnEnergyPctChanged(currentEnergy, maxEnergy);
+     }
+
+}

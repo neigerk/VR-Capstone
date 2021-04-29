23580b1cb1c7632e3a87d5fb96ea0c0007aba769
Zach Parsons <parsonsz@oregonstate.edu>
2021-04-29 16:01:37 -0700

    atack sequence properly decrements energy

--- Assets/Scripts/attackTimer.cs
+++ Assets/Scripts/attackTimer.cs
@@ -7,25 +7,35 @@ public class attackTimer : MonoBehaviour
 {
     public TextMesh timeText;
     public bool activated;
+    public bool unable;
     public float timeRemaining;
+    private PlayerStats playerHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         timeText.text = "";
+        unable = false;
         activated = false;
         timeRemaining = 0;
+        playerHealth = GameObject.Find("PlayerStats").GetComponent<PlayerStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // detects a press of the A button
-        if (OVRInput.Get(OVRInput.Button.One)){
-            activated = true;
-            timeRemaining = 20;
+        if (OVRInput.GetDown(OVRInput.Button.One)){
+            if (playerHealth.GetCurrentEnergy() >= 25){ // player is able to attack
+                activated = true;
+                timeRemaining = 20;
+                playerHealth.ChangeCurrentEnergy(-25);
+            } else{ // player has too little energy/
+                unable = true;
+                timeRemaining = 2;
+            }
         }
-        if (activated){
+        if (activated){ // attack sequence is started
             if (timeRemaining > 0)
             {
                 timeRemaining -= Time.deltaTime;
@@ -35,6 +45,18 @@ public class attackTimer : MonoBehaviour
                 timeText.text = "";
                 timeRemaining = 0;
                 activated = false;
+            }
+        }
+        if (unable){ // player has too little energy
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                timeText.text = "Need 25 or more energy to attack.";
+            } else
+            {
+                timeText.text = "";
+                timeRemaining = 0;
+                unable = false;
             }
         }
     }

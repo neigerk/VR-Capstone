d61ea97fab6bf439f68d66d92dbed286561b8235
Kevin Neiger <neigerk@oregonstate.edu>
2021-04-29 14:24:54 -0700

    Added additional songs.

---
+++ Assets/Scripts/SongArray.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongArray : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    public float[] bpms;
+    private SongCarrier songCarrier;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        songCarrier = GameObject.Find("SongCarrier").GetComponent<SongCarrier>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateSongCarrier(int num)
+    {
+        songCarrier.audioClip = audioClips[num];
+        songCarrier.bpm = bpms[num];
+    }
+}
---
+++ Assets/Scripts/SongCarrier.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SongCarrier : MonoBehaviour
+{
+    private static SongCarrier _SongCarrierInstance;
+
+    public float bpm;
+    public AudioClip audioClip;
+
+    [SerializeField]
+    private GameObject canvas;
+
+    private Scene currentScene;
+
+    private void Awake()
+    {
+        Debug.Log(SceneManager.GetActiveScene().name);
+        currentScene = SceneManager.GetActiveScene();
+        /*
+        if(SceneManager.GetActiveScene().name == "BetaScene")
+        {
+            GameObject.Find("Canvas").GetComponent<AudioSource>().clip = audioClip;
+            GameObject.Find("BPM").GetComponent<BPM>().bpm = bpm;
+        }
+        */
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_SongCarrierInstance != null && _SongCarrierInstance != this)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            _SongCarrierInstance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( currentScene != SceneManager.GetActiveScene())
+        {
+            currentScene = SceneManager.GetActiveScene();
+            UpdateSongs();
+        }
+    }
+    
+    private void UpdateSongs()
+    {
+        Debug.Log(SceneManager.GetActiveScene().name);
+        if (SceneManager.GetActiveScene().name == "BetaScene")
+        {
+            GameObject.Find("Canvas").GetComponent<AudioSource>().clip = audioClip;
+            GameObject.Find("BPM").GetComponent<BPM>().bpm = bpm;
+            GameObject.Find("Canvas").GetComponent<AudioSource>().Play();
+
+
+        }
+    }
+}
